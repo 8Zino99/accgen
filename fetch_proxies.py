9cd687201831e5dc06379156ab254ac3936ce5ec
8Zino99 <larshanekart@gmail.com>
2024-08-27 17:30:38 +0200

    Update fetch_proxies.py

--- fetch_proxies.py
+++ fetch_proxies.py
@@ -1,16 +1,16 @@
 import requests
+import base64  # Füge diesen Import hinzu
 
 def fetch_proxies():
-    url = "https://api.proxyscrape.com/v3/accounts/freebies/scraperapi/request"  # Beispiel-URL, ersetze durch die tatsächliche
+    url = "https://api.proxyscrape.com/v3/accounts/freebies/scraperapi/request"
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Api-Key': 'd2573568-f1c3-4738-88da-f9e1fb0ea714'
+    }
     data = {
         "url": "https://books.toscrape.com/",
         "httpResponseBody": True
     }
-    headers = {
-        'Content-Type': 'application/json',
-        'X-Api-Key': 'd2573568-f1c3-4738-88da-f9e1fb0ea714'  # Dein API-Schlüssel hier
-    }
-
     response = requests.post(url, headers=headers, json=data)
 
     if response.status_code == 200:
@@ -20,7 +20,7 @@ def fetch_proxies():
         else:
             print(base64.b64decode(json_response['data']['httpResponseBody']).decode())
     else:
-        print("Error:", response.status_code)
+        print(f"Failed to fetch proxies. Status code: {response.status_code}")
 
 if __name__ == "__main__":
     fetch_proxies()
