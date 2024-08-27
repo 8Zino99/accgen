8fa4039b5c2543e03a45cfef255a624f5886a837
8Zino99 <larshanekart@gmail.com>
2024-08-27 16:55:30 +0200

    Update fetch_proxies.py

--- fetch_proxies.py
+++ fetch_proxies.py
@@ -1,29 +1,15 @@
 import requests
 
 def fetch_proxies():
-    url = "https://api.proxyscrape.com/v3/accounts/freebies/scraperapi/request"
-    headers = {
-        'Content-Type': 'application/json',
-        'X-Api-Key': 'd2573568-f1c3-4738-88da-f9e1fb0ea714'  # Ersetze dies durch deinen echten API-Key
-    }
-    data = {
-        "url": "https://books.toscrape.com/",
-        "httpResponseBody": True
-    }
-
-    response = requests.post(url, headers=headers, json=data)
+    url = "https://api.proxyscrape.com/v2/account/datacenter_shared/proxy-list?auth=d2573568-f1c3-4738-88da-f9e1fb0ea714&type=getproxies&country[]=all&protocol=http&format=normal&status=all"
+    response = requests.get(url)
 
     if response.status_code == 200:
-        json_response = response.json()
-        proxies = json_response.get('data', {}).get('httpResponseBody', '')
-        if proxies:
-            proxies = proxies.splitlines()
-            with open('proxy.txt', 'w') as file:
-                for proxy in proxies:
-                    file.write(f"{proxy}\n")
-            print("Proxies successfully written to proxy.txt.")
-        else:
-            print("No proxies found.")
+        proxies = response.text.splitlines()
+        with open('proxy.txt', 'w') as file:
+            for proxy in proxies:
+                file.write(f"{proxy}\n")
+        print("Proxies successfully written to proxy.txt.")
     else:
         print(f"Failed to fetch proxies. Status code: {response.status_code}")
 
