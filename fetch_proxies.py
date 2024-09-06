e589e5b153f54987681fd6959b67aba9e943ef81
8Zino99 <larshanekart@gmail.com>
2024-09-06 12:45:27 +0200

    Update fetch_proxies.py

--- fetch_proxies.py
+++ fetch_proxies.py
@@ -1,38 +1,10 @@
-#!/usr/bin/env python3
 import requests
-import base64
-import json
+from lxml.html import fromstring
 
-data = {
-    "url": "https://roblox.com",  # URL zur gewünschten Website
-    "httpResponseBody": True
-}
 
-headers = {
-    'Content-Type': 'application/json',
-    'X-Api-Key': 'd2573568-f1c3-4738-88da-f9e1fb0ea714'  # Dein API-Key
-}
-
-try:
-    response = requests.post('https://api.proxyscrape.com/v3/accounts/freebies/scraperapi/request', headers=headers, json=data)
-    response.raise_for_status()  # Überprüft auf HTTP-Fehler
-except requests.exceptions.RequestException as e:
-    print(f"Request failed: {e}")
-    exit()
-
-print(f"Response Status Code: {response.status_code}")
-
-if response.status_code == 200:
-    try:
-        json_response = response.json()
-        print("API Response Data:")
-        print(json.dumps(json_response, indent=4))  # Zeigt die gesamte API-Antwort an
-        if 'data' in json_response and 'httpResponseBody' in json_response['data']:
-            print("Decoded HTML:")
-            print(base64.b64decode(json_response['data']['httpResponseBody']).decode())
-        else:
-            print("No httpResponseBody in response data.")
-    except (ValueError, KeyError) as e:
-        print(f"Error parsing JSON response: {e}")
-else:
-    print(f"Error: {response.status_code}")
+class ProxyGenerator:
+    def get() -> list[str]:
+        response = requests.get('https://sslproxies.org/')
+        parser = fromstring(response.text)
+        return [":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]]) for i in parser.xpath('//tbody/tr')[:100] \
+            if i.xpath('.//td[7][contains(text(),"yes")]')]
